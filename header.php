45627accce95010265141710a1d41521f353acd5
tpalmer75 <tpalmer75@ctl-3800-studentmac3.lib.byu.edu>
2013-10-08 14:02:02 -0600

    Fonts!

--- header.php
+++ header.php
@@ -34,6 +34,8 @@ if ( !defined('ABSPATH')) exit;
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
+<link rel="stylesheet" type="text/css" href="/sacredgifts/wp-content/fonts/MyFontsWebfontsKit.css">
+
 <?php wp_head(); ?>
 </head>
 
